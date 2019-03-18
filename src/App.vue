37b79edc697d9a84bc5e7ee9eb3bbbd0382a791a
testme2000 <nitin.dixfree@gmail.com>
2019-03-17 19:40:54 -0500

    Initial CheckIn

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Image Day</h1>
+    <h2>Image of today </h2>
+    <image-for-date></image-for-date>
+  </div>
+</template>
+
+<script>
+  import FetchImage from './components/FetchImage.vue'
+  
+export const GLOBAL = {
+  data() {
+      return {
+        NASA_APIKEY : 'vME6LAMD7IhEiy7rDmjfIaG6MhiKbu1MNIqxtqd1',
+        NASA_WEBURL : 'https://api.nasa.gov/planetary/apod?api_key='
+      }
+  }
+}
+
+export default {
+  name: 'app',
+  mixins : [GLOBAL],
+  components: {
+    'ImageForDate' : FetchImage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FetchImage.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div>
+        <h1>Fetching is in progress</h1>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name : 'FetchImage',
+        props : ['fetchdate'],
+        data : function() {
+            return {
+
+            }
+        }
+    }
+</script>
