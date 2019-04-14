f0a30d1e4cf6c877e9dd4e4ad4486eb41cf054e5
testme2000 <nitin.dixfree@gmail.com>
2019-04-14 09:15:48 -0500

    Started implementing Jest with test utils

--- app.test.js
+++ app.test.js
@@ -15,7 +15,20 @@ describe("ImageDay Basic Layout Verification", () => {
     test("App Property Verification", () => {
         // 1. Validate instance
         expect(appWrapper.isVueInstance()).toBeTruthy();
-        // 2. 
+        // 2. Div Tag - Visiblity, count, attribute
+        expect(appWrapper.html().includes("div")).toBe(true);
+        expect(appWrapper.attributes().id).toBe("app");
+        expect(appWrapper.find("div").isVisible()).toBe(true);
+        let divarray = appWrapper.findAll("div");
+        expect(divarray.length).toBe(4);
+        console.log(appWrapper.html());
+        // 6. Table, Row and column Tag - Visiblity, count, attribute
+        expect(appWrapper.html().includes("table")).toBe(true);
+        expect(appWrapper.findAll('table').length).toEqual(1);
+        //expect(appWrapper.classes()).toContain('table table-bordered');
+        //expect(appWrapper.classes('table table-bordered')).toBe(true);
+        
+        
     })
     // 1. Validate instance
     
